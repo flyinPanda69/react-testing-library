d3f2e2e49f9d4565bee792c387a59550bd349ecc
flyinPanda69 <rarockin002@gmail.com>
2023-02-15 00:44:05 +0530

    Added eslint and prettier configuration

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-
 export function replaceCamelWithSpaces(colorName) {
-  return colorName.replace(/\B([A-Z])\B/g, ' $1');
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
 }
 
 function App() {
-  const [buttonColor, setButtonColor] = useState('MediumVioletRed');
+  const [buttonColor, setButtonColor] = useState("MediumVioletRed");
   const [disabled, setIsDisabled] = useState(false);
-  const newButtonColor = buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
+  const newButtonColor =
+    buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
 
-  const onButtonClick = (e) => {
-
-  }
-  const onCheckBoxClick = (e ) => {
+  const onCheckBoxClick = (e) => {
     setIsDisabled(e.target.checked);
-  }
+  };
   return (
     <div>
       <button
         disabled={disabled}
-        style={{ backgroundColor: disabled ? 'grey' : buttonColor }}
+        style={{ backgroundColor: disabled ? "grey" : buttonColor }}
         onClick={() => setButtonColor(newButtonColor)}
       >
         Change to {replaceCamelWithSpaces(newButtonColor)}
